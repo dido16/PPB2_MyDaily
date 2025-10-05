364600d666168bb55f4c039dd36cf014d3908b4b
dido16 <didofajar16@gmail.com>
2025-10-06 06:30:25 +0700

    fix: update FirebaseAuth dan Kotlin compatibility

--- app/src/main/java/com/example/mydaily/MainActivity.kt
+++ app/src/main/java/com/example/mydaily/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.example.mydaily
 
-import androidx.credentials.CredentialManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.credentials.exceptions.NoCredentialException
 import androidx.lifecycle.lifecycleScope
 import com.example.mydaily.databinding.ActivityMainBinding
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.auth
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -29,20 +26,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         credentialManager = CredentialManager.create(this)
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance() //
 
         registerEvents()
     }
 
-    fun registerEvents() {
+    private fun registerEvents() {
         binding.btnLogin.setOnClickListener {
             lifecycleScope.launch {
                 val request = prepareRequest()
@@ -51,40 +51,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun prepareRequest(): GetCredentialRequest {
-        val serverClientId = "824354432771-9oj8bqgi98so7sfv1386mq77ol3l27ko.apps.googleusercontent.com"
+    private fun prepareRequest(): GetCredentialRequest {
+        val serverClientId =
+            "824354432771-9oj8bqgi98so7sfv1386mq77ol3l27ko.apps.googleusercontent.com"
 
-        val googleIdOption = GetGoogleIdOption
-            .Builder()
+        val googleIdOption = GetGoogleIdOption.Builder()
             .setFilterByAuthorizedAccounts(false)
             .setServerClientId(serverClientId)
             .build()
 
-        return GetCredentialRequest
-            .Builder()
+        return GetCredentialRequest.Builder()
             .addCredentialOption(googleIdOption)
             .build()
     }
 
-    suspend fun loginByGoogle(request: GetCredentialRequest) {
+    private suspend fun loginByGoogle(request: GetCredentialRequest) {
         try {
-            val result = credentialManager.getCredential(
-                context = this,
-                request = request
-            )
+            val result = credentialManager.getCredential(context = this, request = request)
             val credential = result.credential
             val idToken = GoogleIdTokenCredential.createFrom(credential.data)
 
             firebaseLoginCallback(idToken.idToken)
         } catch (exc: NoCredentialException) {
-            Toast.makeText(this, "Login gagal :" + exc.message, Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Login gagal: ${exc.message}", Toast.LENGTH_LONG).show()
         } catch (exc: Exception) {
-            Toast.makeText(this, "Login gagal :" + exc.message, Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Login gagal: ${exc.message}", Toast.LENGTH_LONG).show()
         }
-
     }
 
-    fun firebaseLoginCallback(idToken: String) {
+    private fun firebaseLoginCallback(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
@@ -95,6 +90,4 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
-
 }
-
